f26e92d05013a6b93fa185d3a9ceecad64a74040
Mike Web <mweb@mikeweb.tech>
2023-03-15 14:46:40 -0400

    Slug::generate() EntityInterface support

--- src/Utility/Slug.php
+++ src/Utility/Slug.php
@@ -2,6 +2,7 @@
 
 namespace MikeWeb\CakeText\Utility;
 
+use Cake\Datasource\EntityInterface;
 use Cake\Utility\Inflector;
 use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\NotFoundExceptionInterface;
@@ -9,17 +10,33 @@ use voku\helper\StopWordsLanguageNotExists;
 
 class Slug
 {
+    const SEPARATOR = '-';
+
     /**
      * @throws StopWordsLanguageNotExists
      * @throws ContainerExceptionInterface
      * @throws NotFoundExceptionInterface
      */
-    public static function generate(string $string, array $options=[]): string
+    public static function generate(EntityInterface|string $source, array $options=[]): string
     {
         $options += [
-            'case'      => Text::CASE_LOWER,
-            'strip'     => false,
+            'case'          => Text::CASE_LOWER,
+            'field'         => 'name',
+            'strip'         => false,
+            'prefix'        => null,
+            'replacement'   => static::SEPARATOR,
         ];
+
+        $string = ($source instanceof EntityInterface) ?
+            $source->get($options['field']) :
+            (string)$source;
+
+        if (
+            $source instanceof EntityInterface
+            && false !== filter_var($source->get('id'), FILTER_VALIDATE_INT)
+        ) {
+            $options['prefix'] = $source->get('id');
+        }
 
         if ($options['strip']) {
             $string = preg_replace(
@@ -37,14 +54,16 @@ class Slug
             $string
         );
 
-        return mb_convert_case(
-            \Cake\Utility\Text::slug(
-                Inflector::underscore(
-                    trim($string, "_- \t\n\r\0\x0B")
-                )
-            ),
-            $options['case'],
-            '8bit'
+        $slug = \Cake\Utility\Text::slug(
+            Inflector::underscore(
+                trim($string, "_- \t\n\r\0\x0B")
+            )
         );
+
+        if ($options['prefix']) {
+            $slug = (string)$options['prefix'] . $options['replacement'] . $slug;
+        }
+
+        return mb_convert_case($slug, $options['case'], '8bit');
     }
 }
